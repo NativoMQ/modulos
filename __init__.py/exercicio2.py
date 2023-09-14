d55a23d6e4b5be73cce2c6098b79e632958a6ce4
NativoMQ <mq376446@gmail.com>
2023-09-14 19:39:02 -0400

    ab

---
+++ __init__.py/exercicio2.py
@@ -0,0 +1,3 @@
+#criar 2 funcoes a primeira deve aceitar um numero e deve retornar o dobro desse numero
+# a segunda deve aceitar um numero e retornar o quadrado desse numero 
+# em seguida chame a primeira funcao dentro da segunda para retornar o quadrado do dobro de um numero
